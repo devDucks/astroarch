51fd160fda1d671664a181ca51bd1db08cf3161f
Mattia Procopio <promat85@gmail.com>
2022-02-06 18:47:45 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -43,7 +43,7 @@ ln -s /home/astronaut/.astroarch/systemd/autologin.conf /etc/sddm.conf.d/autolog
 ln -s /home/astronaut/.astroarch/systemd/novnc.service /usr/lib/systemd/system/novnc.service
 ln -s /home/astronaut/.astroarch/systemd/x11vnc.service /home/astronaut/.config/systemd/user/default.target.wants/x11vnc.service
 ln -s /home/astronaut/.astroarch/systemd/x11vnc.service /home/astronaut/.config/systemd/user
-ln -s /home/astronaut/.configs/20-headless.conf /usr/share/X11/xorg.conf.d/20-headless.conf
+ln -s /home/astronaut/.astroarch/configs/20-headless.conf /usr/share/X11/xorg.conf.d/20-headless.conf
 
 # Enable now all services
 systemctl enable sddm.service novnc.service dhcpcd.service NetworkManager.service
