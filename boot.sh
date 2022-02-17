d515403fb4303c7f28e988a7aed7ff7170a25812
Mattia Procopio <promat85@gmail.com>
2022-02-17 18:03:11 +0100

    Commit samba changes
    Also add custom pacman repo

--- boot.sh
+++ boot.sh
@@ -1,13 +1,27 @@
-#TODO here, parallelize pacman downloads
+# Parallelize pacman download to 5
 sed -i 's/#ParallelDownloads = 5/ParallelDownloads=5/g' /etc/pacman.conf
+
+# Add astroarch pacman repo to pacman.conf
+cat <<EOF >> /etc/pacman.conf
+[astroarch]
+SigLevel = Optional TrustAll
+Server = http://astroarch.astromatto.com:9000/$arch
+EOF
+
+# Bootstrap pacman-key
 pacman-key --init && pacman-key --populate archlinuxarm
+
+# Update all packages now
 pacman -Syu --noconfirm
+
+# Install just 2 packages for the next actions
 pacman -S wget sudo git --noconfirm
 
 # create user astro with home, add it to wheel
 useradd -G wheel -m astronaut
 passwd astronaut
 
+# Pull the brain repo, this will be used for scripting out the final image
 su astronaut -c "git clone https://github.com/MattBlack85/astroarch.git /home/astronaut/.astroarch"
 
 pacman -Syu base-devel go zsh plasma-desktop sddm networkmanager xf86-video-dummy \
@@ -19,7 +33,7 @@ pacman -Syu base-devel go zsh plasma-desktop sddm networkmanager xf86-video-dumm
 	qt5-websockets qtkeychain stellarsolver xf86-video-fbdev \
 	extra-cmake-modules kf5 eigen inetutils xplanet plasma-nm \
 	dhcp dnsmasq x11vnc gedit dolphin uboot-tools usbtools \
-	cloud-guest-utils --noconfirm
+	cloud-guest-utils samba yay --noconfirm
 
 # Allow wheelers to sudo without password to install packages
 sed -i 's/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/g' /etc/sudoers
--- build.sh
+++ build.sh
@@ -1,18 +1,11 @@
-# Install yay
-cd /home/astronaut
-su astronaut -c "git clone https://aur.archlinux.org/yay.git"
-cd yay/
-su astronaut -c "makepkg -s"
-pacman -U yay*.tar.xz --noconfirm
-cd .. && rm -rf yay/
-
 # install oh-my-zsh and set the default shell to zsh
 chsh -s /usr/bin/zsh astronaut
 rm /home/astronaut/.bash*
 cd /home/astronaut
 ZSH=/home/astronaut/.oh-my-zsh sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 chown astronaut:astronaut .oh-my-zsh/
-# copy a zsh config for astronaut
+
+# Link a zsh config for astronaut
 ln -s /home/astronaut/.astroarch/configs/.zshrc /home/astronaut/.zshrc
 
 su astronaut -c "yay -S --noremovemake --nodiffmenu --answerclean 4 gsc"
@@ -30,7 +23,8 @@ sleep 5
 
 # Create the hotspot and set autoconnect to true
 nmcli device wifi hotspot ifname wlan0 ssid AstroArch password "astronomy"
-sed -i 's/autoconnect=false/autoconnect=true/g' /etc/NetworkManager/system-connections/Hotspot.nmconnlection
+sed -i 's/autoconnect=false/autoconnect=true/g' /etc/NetworkManager/system-connections/Hotspot.nmconnection
+nmcli connection modify AstroArch connection.autoconnect-priority -100
 
 # Create Xauthority for x11vnc
 su astronaut -c "touch /home/astronaut/.Xauthority"
@@ -45,8 +39,10 @@ ln -s /home/astronaut/.astroarch/systemd/x11vnc.service /home/astronaut/.config/
 ln -s /home/astronaut/.astroarch/systemd/x11vnc.service /home/astronaut/.config/systemd/user
 ln -s /home/astronaut/.astroarch/configs/20-headless.conf /usr/share/X11/xorg.conf.d/20-headless.conf
 ln -s /home/astronaut/.astroarch/systemd/resize_once.service /etc/systemd/system/resize_once.service
+ln -s /home/astronaut/.astroarch/configs/smb.conf /etc/samba/smb.conf
+
 # Enable now all services
-systemctl enable sddm.service novnc.service dhcpcd.service NetworkManager.service avahi-daemon.service resize_once.service
+systemctl enable sddm.service novnc.service dhcpcd.service NetworkManager.service avahi-daemon.service resize_once.service nmb.service smb.service
 
 # Take sudoers to the original state
 sed -i 's/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/g' /etc/sudoers
