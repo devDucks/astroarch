b9ac50149c94f3fb77db28fae3d4607d8e4ee5ba
Mattia Procopio <promat85@gmail.com>
2023-01-14 20:28:20 +0100

    arch kernel breaks sddm+plasma, revert to rpi kernel

--- boot.sh
+++ boot.sh
@@ -33,7 +33,7 @@ pacman -Syu base-devel pipewire-jack gnu-free-fonts pipewire-media-session \
 	xplanet plasma-nm dhcp dnsmasq x11vnc gedit plasma-systemmonitor \
 	dolphin uboot-tools usbutils cloud-guest-utils samba paru \
 	websockify novnc astrometry.net gsc kstars phd2 \
-	indi-3rdparty-libs indi-3rdparty-drivers --noconfirm --ask 4
+	indi-3rdparty-libs indi-3rdparty-drivers linux-rpi linux-rpi-headers --noconfirm --ask 4
 
 # Allow wheelers to sudo without password to install packages
 sed -i 's/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/g' /etc/sudoers
