bdb66d58e94006a5fde208c85905f3ed555558f9
Mattia Procopio <promat85@gmail.com>
2023-02-01 17:10:41 +0100

    Fix 1.4 script

--- scripts/1.4.sh
+++ scripts/1.4.sh
@@ -1,2 +1,1 @@
-sudo rm -r /usr/share/webapps/novnc/app/images/icons
-sudo ln -s /home/astronaut/.astroarch/assets/icons /usr/share/webapps/novnc/app/images/icons
+sudo cp /home/astronaut/.astroarch/assets/icons/* /usr/share/webapps/novnc/app/images/icons/
