193793ac5115d879fc2e6a74fcdd0ea3a4385c94
Mattia Procopio <promat85@gmail.com>
2023-03-31 16:54:01 +0200

    Fix icons in 1.5 script

--- scripts/1.5.sh
+++ scripts/1.5.sh
@@ -5,3 +5,10 @@ bash /home/astronaut/.astroarch/scripts/1.4.sh
 sudo pacman -Fy
 sudo pacman -R alacritty
 sudo pacman -S konsole
+
+# Add Konsole icon to desktop and remove Alacritty one
+su astronaut -c "cp /home/astronaut/.astroarch/desktop/org.kde.konsole.desktop /home/astronaut/Desktop"
+rm /home/astronaut/Desktop/Alacritty.desktop
+
+# Make the icons executable so there will be no ! on the first boot
+chmod +x /home/astronaut/Desktop/org.kde.konsole.desktop
