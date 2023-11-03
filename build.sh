9b1478724f79c09f22eab3a0ed0d2569774c9752
Mattia Procopio <promat85@gmail.com>
2023-11-03 15:29:33 +0100

    Try to fix i2c on raspberry

--- build.sh
+++ build.sh
@@ -90,7 +90,8 @@ echo "127.0.1.1          astroarch" >> /etc/hosts
 su astronaut -c "cp /home/astronaut/.astroarch/configs/kscreenlockerrc /home/astronaut/.config/kscreenlockerrc"
 
 # If we are on a raspberry let's adjust /boot/config.txt
-if [ $(cat /proc/cpuinfo | grep -c Raspberry) -eq 1 ]; then
+raspi=$(cat /proc/cpuinfo | grep -c Raspberry)
+if [ $raspi -eq 1 ]; then
     echo dtparam=i2c_arm=on >> /boot/config.txt
     echo dtoverlay=i2c-rtc >> /boot/config.txt
     echo i2c-dev > /etc/modules-load.d/raspberrypi.conf
