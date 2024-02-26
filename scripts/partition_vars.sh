8187fbfc0a80941511daa4c7c5da6b44614345b4
Mattia Procopio <promat85@gmail.com>
2024-02-26 16:20:57 +0100

    Add script for resizing partitions at boot

---
+++ scripts/partition_vars.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# WARNING: This script doesn't work with encrypted/lvm/luks partitions
+
+set -e
+
+partition=$( mount | grep -E  "on / type [a-z]+" | awk '{print $1}')
+
+if [ $(echo $partition | grep -c sda) -eq 1 ]; then
+    ROOT_DISK=$(echo | sed 's/[0-9]//g')
+    ROOT_PARTITION=$(echo $partition | grep -Eo [0-9]+)
+    MEDIA=usb
+elif [ $(echo $partition | egrep  '(mmcblk|nvme)' ) -eq 1 ] ]; then
+    ROOT_DISK=$(echo $partition | cut -d "p" -f1)
+    ROOT_PARTITION=$(echo $partition | cut -d "p" -f2)
+    MEDIA=other
+fi
+
+if [ -z $ROOT_DISK ] || [ -z $ROOT_PARTITION ] || [ -z $MEDIA ]; then
+    exit 2
+fi
+
+# Run now growpart on the root disk to grow the partition
+growpart -N \${ROOT_DISK} \${ROOT_PARTITION} && growpart \${ROOT_DISK} \${ROOT_PARTITION} || exit 3
+
+# Resize the root partition to full available space
+if [ $MEDIA == "other"]; then
+    resize2fs \${ROOT_DISK}\p${ROOT_PARTITION} || exit 4
+elif [ $MEDIA = "usb" ]; then
+    resize2fs \=${ROOT_DISK}${ROOT_PARTITION} || exit 4
+else
+    echo "unknown media $MEDIA, erroring" && exit 4
+fi
