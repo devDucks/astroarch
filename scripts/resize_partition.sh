87fc9006b1a571d2a3aedf4deb3f6997544345c1
Mattia Procopio <promat85@gmail.com>
2024-02-26 17:43:03 +0100

    fix resize script

--- scripts/resize_partition.sh
+++ scripts/resize_partition.sh
@@ -10,7 +10,7 @@ if [ $(echo $partition | grep -c sda) -eq 1 ]; then
     ROOT_DISK=$(echo | sed 's/[0-9]//g')
     ROOT_PARTITION=$(echo $partition | grep -Eo [0-9]+)
     MEDIA=usb
-elif [ $(echo $partition | egrep  '(mmcblk|nvme)' ) -eq 1 ] ]; then
+elif [ $(echo $partition | grep -cE "(mmcblk|nvme)" ) -eq 1 ] ]; then
     ROOT_DISK=$(echo $partition | cut -d "p" -f1)
     ROOT_PARTITION=$(echo $partition | cut -d "p" -f2)
     MEDIA=other
