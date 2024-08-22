88d900b13b0f50745684aa2d5a9385ca9b040d97
Mattia Procopio <promat85@gmail.com>
2024-08-22 10:24:52 +0200

    Add QHY lib removal before uprading for 1.9.2

--- scripts/1.9.2.sh
+++ scripts/1.9.2.sh
@@ -5,3 +5,8 @@ if [ ! -f /etc/modprobe.d/brcmfmac.conf ]; then
     sudo bash -c "echo \"options brcmfmac feature_disable=0x82000\" > /etc/modprobe.d/brcmfmac.conf"
     echo "PLEASE REMEMBER TO REBOOT TO MAKE THE WIFI WORKING"
 fi
+
+# This will fix the problem with qhyccd.so.20
+if [ -L /usr/lib/libqhyccd.so.20 ]; then
+    sudo unlink /usr/lib/libqhyccd.so.20
+fi
