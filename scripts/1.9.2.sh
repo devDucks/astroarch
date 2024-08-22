7a7b790b544a355ce61a86faad2c91517ebf086a
Mattia Procopio <promat85@gmail.com>
2024-08-22 10:46:11 +0200

    New approach for QHY conflict fix

--- scripts/1.9.2.sh
+++ scripts/1.9.2.sh
@@ -8,5 +8,7 @@ fi
 
 # This will fix the problem with qhyccd.so.20
 if [ -L /usr/lib/libqhyccd.so.20 ]; then
-    sudo unlink /usr/lib/libqhyccd.so.20
+    yes | LC_ALL=en_US.UTF-8 sudo pacman -Sy
+    yes | LC_ALL=en_US.UTF-8 sudo pacman -R indi-3rdparty-libs indi-3rdparty-drivers
+    yes | LC_ALL=en_US.UTF-8 sudo pacman -S indi-3rdparty-libs indi-3rdparty-drivers
 fi
