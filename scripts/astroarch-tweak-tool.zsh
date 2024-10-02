8bd77548c1fdc15f26e8667d3484a61babb646d4
sc74 <scarlin74@gmail.com>
2024-10-02 12:37:13 +0200

    Add usefull commands

--- scripts/astroarch-tweak-tool.zsh
+++ scripts/astroarch-tweak-tool.zsh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-result=$(kdialog --menu "Select action" 1 "Update-astroarch" 2 "Set GPS" 3 "Set Bluetooth" 4 "Set FTP" --title "AstroArch Tweak Tool")
+result=$(kdialog --menu "Select action" 1 "Update-astroarch" 2 "Set GPS" 3 "Set Bluetooth" 4 "Set FTP" 5 "Rollback Kstars/Indi" 6 "Install Kstars/Indi stable/bleeding-edge" --title "AstroArch Tweak Tool")
 source ~/.zshrc
 case $result in
     1)
@@ -41,6 +41,31 @@ case $result in
                 ;;
             OFF)
                 ftp_off
+                ;;
+        esac
+        ;;
+    5)
+        rollback=$(kdialog --combobox "Select action for rollback" "Full-Kstars-Indi" "Indi" "Kstars" --title "AstroArch Rollback")
+        case $rollback in
+            Full-Kstars-Indi)
+                astro-rollback-full
+                ;;
+            Indi)
+                astro-rallback-indi
+                ;;
+            Kstars)
+                astro-rollback-kstars
+                ;;
+        esac
+        ;;
+    6)
+        install_kstars=$(kdialog --combobox " Select the Kstars installation version" "Bleeding-edge" "Stable" --title "AstroArch Install Kstars")
+        case $install_kstars in
+            Bleeding-edg)
+                use-astro-bleeding-edge
+                ;;
+            Stable)
+                use-astro-stable
                 ;;
         esac
         ;;
