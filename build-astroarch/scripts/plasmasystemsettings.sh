f87e15ad1e30f06e305220dfd92851add0ab1ce0
sc74 <scarlin74@gmail.com>
2025-03-20 21:40:23 +0000

    Bootstrap process for building AstroArch from existing AstroArch update4

--- build-astroarch/scripts/plasmasystemsettings.sh
+++ build-astroarch/scripts/plasmasystemsettings.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-lookandfeeltool -a org.kde.breezedark.desktop
+kwriteconfig6 --file "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc" --group 'Containments' --group '1' --key 'wallpaperplugin' "org.kde.slideshow"
+
+sleep 1
 
 kwriteconfig6 --file "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc" --group 'Containments' --group '1' --group 'Wallpaper' --group 'org.kde.slideshow' --group 'General' --key 'SlidePaths' "/home/astronaut/Pictures/wallpapers/"
 
-kwriteconfig6 --file "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc" --group 'Containments' --group '1' --key 'wallpaperplugin' "org.kde.slideshow"
+sleep 1
+
+lookandfeeltool -a org.kde.breezedark.desktop
+
+sleep 1
 
 systemctl restart --user plasma-plasmashell
+
+sleep 1
