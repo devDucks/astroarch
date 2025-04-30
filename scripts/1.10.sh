b04fdb491f080d9971602b455ff40c54aca7e160
Mattia Procopio <promat85@gmail.com>
2025-04-30 15:13:57 +0200

    Prepare the script to update to 1.10 (empty scaffold)

---
+++ scripts/1.10.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# First run 1.9.3.sh to be sure that old changes will be applied
+bash /home/astronaut/.astroarch/scripts/1.9.3.sh
+
+# Things to do
+
+# check pacman.conf and disable download timeout
+
+# allow astronaut to sudo wuth no pass
+
+# copy over xorg.conf (remove and cp again)
+
+# copy over config.txt (remove and cp again? This will drop changes that people did manually)
