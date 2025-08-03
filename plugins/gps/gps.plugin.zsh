45aafdeeecb491f1d96cf7dc3abb6f55241bffba
sc74 <scarlin74@gmail.com>
2025-08-03 20:15:03 +0000

    Improvement of the config.txt, x0vncserver,2.01.sh and gps.plugin.zsh files + script to reset the wifi driver (error -52)

--- plugins/gps/gps.plugin.zsh
+++ plugins/gps/gps.plugin.zsh
@@ -7,61 +7,62 @@ function gps_on()
 	yes | LC_ALL=en_US.UTF-8 sudo pacman -S gpsd
     echo "GPS packages installed!"
     fi
-
+    sudo systemctl enable gpsd.service --now
     sudo sh -c "echo 'refclock SHM 0 offset 0.5 delay 0.2 refid NMEA' >> /etc/chrony.conf"
     sudo sh -c "echo 'driftfile /var/lib/chrony/drift' >> /etc/chrony.conf"
     sudo rm /etc/default/gpsd
     sudo touch /etc/default/gpsd
     sudo sh -c 'echo "#Default settings for gpsd." >> /etc/default/gpsd'
 
-    if [ -e /dev/ttyACM0 ] && timeout 2s cat /dev/ttyACM0 | head -n 1 > /dev/null; then
-    sudo sh -c 'echo "START_DAEMON=\""false\" >> /etc/default/gpsd'
-    sudo sh -c 'echo "GPSD_OPTIONS=\""-n\" >> /etc/default/gpsd'
-    sudo sh -c 'echo "DEVICES=\""/dev/ttyACM0\" >> /etc/default/gpsd'
-    sudo sh -c 'echo "USBAUTO=\""false\" >> /etc/default/gpsd'
-    sudo systemctl enable gpsd.service --now
-    echo "GPS server USB /dev/ttyACM0 turned ON and enabled to autostart at every boot"
-    return 1
-    else
-    echo "/dev/ttyACM0 not exist"
-    fi
+    gps_info=$(gpspipe -w -n 10 2>/dev/null)
+    if [[ -n "$gps_info" ]]; then
+    device_path=$(echo "$gps_info" | jq -r 'select(.class=="DEVICES") | .devices[0].path' 2>/dev/null)
+        if [[ -n "$device_path" && "$device_path" != "null" ]]; then
+        echo "The GPS device is mounted on the path : $device_path"
+            if [ $device_path="/dev/ttyACM0" ]; then
+                sudo sh -c 'echo "START_DAEMON=\""false\" >> /etc/default/gpsd'
+                sudo sh -c 'echo "GPSD_OPTIONS=\""-n\" >> /etc/default/gpsd'
+                sudo sh -c 'echo "DEVICES=\""/dev/ttyACM0\" >> /etc/default/gpsd'
+                sudo sh -c 'echo "USBAUTO=\""false\" >> /etc/default/gpsd'
+                sudo systemctl restart gpsd.service --now
+                echo "GPS server USB /dev/ttyACM0 turned ON and enabled to autostart at every boot"
+                return 1
+            elif [ $device_path="/dev/ttyAMA0" ]; then
+                sudo sh -c 'echo "START_DAEMON=\""false\" >> /etc/default/gpsd'
+                sudo sh -c 'echo "GPSD_OPTIONS=\""-n\" >> /etc/default/gpsd'
+                sudo sh -c 'echo "DEVICES=\""/dev/ttyAMA0\" >> /etc/default/gpsd'
+                sudo sh -c 'echo "USBAUTO=\""false\" >> /etc/default/gpsd'
+                sudo systemctl restart gpsd.service --now
+                echo "GPS server USB /dev/ttyAMA0 turned ON and enabled to autostart at every boot"
+                return 1
+            elif [ $device_path="/dev/ttyS0" ]; then
+                sudo sh -c 'echo "START_DAEMON=\""false\" >> /etc/default/gpsd'
+                sudo sh -c 'echo "GPSD_OPTIONS=\""-n\" >> /etc/default/gpsd'
+                sudo sh -c 'echo "DEVICES=\""/dev/ttyS0\" >> /etc/default/gpsd'
+                sudo sh -c 'echo "USBAUTO=\""false\" >> /etc/default/gpsd'
+                sudo systemctl restart gpsd.service --now
+                echo "GPS server USB /dev/ttyS0 turned ON and enabled to autostart at every boot"
+                return 1
+            else
+                echo ""
+            fi
+        else
+        echo " gpsd does not appear to have any active devices"
+        fi
 
-    if [ -e /dev/ttyAMA0 ] && timeout 2s cat /dev/ttyAMA0 | head -n 1 > /dev/null; then
-    sudo sh -c 'echo "START_DAEMON=\""true\" >> /etc/default/gpsd'
-    sudo sh -c 'echo "GPSD_OPTIONS=\""\" >> /etc/default/gpsd'
-    sudo sh -c 'echo "DEVICES=\""/dev/ttyAMA0\" >> /etc/default/gpsd'
-    sudo sh -c 'echo "USBAUTO=\""true\" >> /etc/default/gpsd'
-    sudo systemctl enable gpsd.service --now
-    echo "GPS server UART PL011 /dev/ttyACMA0 turned ON and enabled to autostart at every boot"
-    return 1
-    else
-    echo "/dev/ttyACMA0 not exist"
-    fi
+        echo "Please enter your GPS point /dev (eg: /dev/ttyxx):"
+        read device
 
-    if [ -e /dev/ttyS0 ] && timeout 2s cat /dev/ttyS0 | head -n 1 > /dev/null; then
-    sudo sh -c 'echo "START_DAEMON=\""true\" >> /etc/default/gpsd'
-    sudo sh -c 'echo "GPSD_OPTIONS=\""\" >> /etc/default/gpsd'
-    sudo sh -c 'echo "DEVICES=\""/dev/ttyS0\" >> /etc/default/gpsd'
-    sudo sh -c 'echo "USBAUTO=\""true\" >> /etc/default/gpsd'
-    sudo systemctl enable gpsd.service --now
-    echo "GPS server mini UART /dev/ttyS0 turned ON and enabled to autostart at every boot"
-    return 1
-    else
-    echo "/dev/ttyS0 not exist"
-    fi
-
-    echo "Please enter your GPS point /dev (eg: /dev/ttyxx):"
-    read device
-
-    if [ -e "$device" ] && timeout 2s cat "$device" | head -n 1 > /dev/null; then
-    sudo sh -c 'echo "START_DAEMON=\""true\" >> /etc/default/gpsd'
-    sudo sh -c 'echo "GPSD_OPTIONS=\""\" >> /etc/default/gpsd'
-    sudo tee -a /etc/default/gpsd <<< 'DEVICES="'$device'"'
-    sudo sh -c 'echo "USBAUTO=\""true\" >> /etc/default/gpsd'
-    sudo systemctl enable gpsd.service --now
-    echo "GPS server $device turned ON and enabled to autostart at every boot"
-    else
-    echo "No GPS found"
+        if [ -e "$device" ] && timeout 2s cat "$device" | head -n 1 > /dev/null; then
+        sudo sh -c 'echo "START_DAEMON=\""true\" >> /etc/default/gpsd'
+        sudo sh -c 'echo "GPSD_OPTIONS=\""\" >> /etc/default/gpsd'
+        sudo tee -a /etc/default/gpsd <<< 'DEVICES="'$device'"'
+        sudo sh -c 'echo "USBAUTO=\""true\" >> /etc/default/gpsd'
+        sudo systemctl enable gpsd.service --now
+        echo "GPS server $device turned ON and enabled to autostart at every boot"
+        else
+        echo "No GPS found"
+        fi
     fi
 }
 
