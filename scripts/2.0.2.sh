5a0683efaeb7150d7a7edd9aa9eaba98889e88d1
Stéphane Carlin <scarlin74@gmail.com>
2025-08-22 12:41:21 +0200

    Removed 2.0.1.sh relaunch

--- scripts/2.0.2.sh
+++ scripts/2.0.2.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-# First run 2.0.1.sh to be sure that old changes will be applied
-bash /home/astronaut/.astroarch/scripts/2.0.1.sh
-
 # Disable wifi powervaving permanently
 sudo cp /home/astronaut/.astroarch/configs/81-wifi-powersave.rules /etc/udev/rules.d/81-wifi-powersave.rules
