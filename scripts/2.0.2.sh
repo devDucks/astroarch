6d7876c689e8307a3b8e3e85d4486c5f2b5b12d7
Mattia Procopio <promat85@gmail.com>
2025-09-05 17:13:51 +0200

    Add retro-run for 2.0.2 script

--- scripts/2.0.2.sh
+++ scripts/2.0.2.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+bash /home/astronaut/.astroarch/scripts/2.0.1.sh
+
 # Disable wifi powervaving permanently
 sudo cp /home/astronaut/.astroarch/configs/81-wifi-powersave.rules /etc/udev/rules.d/81-wifi-powersave.rules
