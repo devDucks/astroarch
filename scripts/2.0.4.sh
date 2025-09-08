66fadcaebed80d3113416f71b3b28dc01c107968
Stéphane Carlin <scarlin74@gmail.com>
2025-09-08 09:21:18 +0200

    Difference between XRDP and VNC (#179)

---
+++ scripts/2.0.4.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+bash /home/astronaut/.astroarch/scripts/2.0.3.sh
+
+# Install Astroarch-onboarding
+
+sudo pacman -S astroarch-onboarding
+cp /home/astronaut/.astroarch/desktop/AstroArch-onboarding.desktop /home/astronaut/Desktop/AstroArch-onboarding
+
+
+
+
+
