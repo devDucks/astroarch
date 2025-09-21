f107716cdec2f122d0b0e31c2e10ab4fbfb4ca97
Mattia Procopio <promat85@gmail.com>
2025-09-21 17:10:41 +0200

    Fix 2.0.3 script idempotency

--- scripts/2.0.3.sh
+++ scripts/2.0.3.sh
@@ -7,24 +7,28 @@ sudo sed -i -E 's/^[#]?X11Forwarding.*/X11Forwarding yes/' /etc/ssh/sshd_config
 
 # Xrdp
 paru -Sy xrdp xorgxrdp --noconfirm
-chmod +x /home/astronaut/.astroarch/configs/startwm.sh
 sudo mv /etc/xrdp/startwm.sh /etc/xrdp/startwm.sh-old
 sudo ln -sfn /home/astronaut/.astroarch/configs/startwm.sh /etc/xrdp/startwm.sh
 sudo ln -sfn /home/astronaut/.astroarch/configs/Xwrapper.config /etc/xrdp/Xwrapper.config
-sudo cp /home/astronaut/.astroarch/configs/50-udiskie.rules /etc/polkit-1/rules.d/50-udiskie.rules
-sudo cp /home/astronaut/.astroarch/configs/50-networkmanager.rules /etc/polkit-1/rules.d/50-networkmanager.rules
+sudo ln -sfn /home/astronaut/.astroarch/configs/50-udiskie.rules /etc/polkit-1/rules.d/50-udiskie.rules
+sudo ln -sfn /home/astronaut/.astroarch/configs/50-networkmanager.rules /etc/polkit-1/rules.d/50-networkmanager.rules
 sudo systemctl enable --now xrdp xrdp-sesman
 
 # Disable do not disturb mode
 if [ ! -f /home/astronaut/.config/plasmanotifyrc ]; then
-cat <<EOF >/home/astronaut/.config/plasmanotifyrc
+  cat <<EOF >/home/astronaut/.config/plasmanotifyrc
 [DoNotDisturb]
 WhenFullscreen=false
 WhenScreensMirrored=false
 EOF
 else
-sed -i '$a\\[DoNotDisturb]\nWhenFullscreen=false\nWhenScreensMirrored=false' /home/astronaut/.config/plasmanotifyrc
+  # Only add the section if it doesn't exist
+  if ! grep -q "^\[DoNotDisturb\]" /home/astronaut/.config/plasmanotifyrc; then
+    cat <<EOF >>/home/astronaut/.config/plasmanotifyrc
+
+[DoNotDisturb]
+WhenFullscreen=false
+WhenScreensMirrored=false
+EOF
+  fi
 fi
-
-
-
