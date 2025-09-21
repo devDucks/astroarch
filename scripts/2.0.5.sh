bdacab92d5472ae2c245d6e847b30f10c107ab98
Mattia Procopio <promat85@gmail.com>
2025-09-21 17:02:16 +0200

    Make 2.0.5 script idempotent

--- scripts/2.0.5.sh
+++ scripts/2.0.5.sh
@@ -2,10 +2,7 @@
 
 bash /home/astronaut/.astroarch/scripts/2.0.4.sh
 
-# Increases the xrdp buffet
-sudo sed -i 's|#tcp_send_buffer_bytes=32768|tcp_send_buffer_bytes= 4194304|g' /etc/xrdp/xrdp.ini
-
-
-
-
-
+# Increases the xrdp buffer
+if grep -q "^#tcp_send_buffer_bytes=" /etc/xrdp/xrdp.ini; then
+    sudo sed -i 's|^#tcp_send_buffer_bytes=.*|tcp_send_buffer_bytes=419|g' /etc/xrdp/xrdp.ini
+fi
