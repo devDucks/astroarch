78563505ba518af0b845532a720a6b04f9f1e1eb
sc74 <sc74@gmail.com>
2025-10-08 11:20:11 +0100

    Minor update

--- plugins/power_max_current/power_max_current.plugin.zsh
+++ plugins/power_max_current/power_max_current.plugin.zsh
@@ -3,8 +3,7 @@ function power_max_current()
     if grep 'usb_max_current_enable=1' /boot/config.txt > /dev/null ; then
     echo "Maximum current is already activated"
     else
-    sudo sed -i '$ausb_max_current_enable=1' /boot/config.txt
+    sudo sed -i '/\[pi5\]/a usb_max_current_enable=1' /boot/config.txt
     echo "Maximum current activation for the Pi 5 USB ports is done after restart"
     fi
 }
-
