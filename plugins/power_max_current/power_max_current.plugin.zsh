f2383fc9fe0bcb56fde2dcaa483e8fbfb03a39a5
sc74 <sc74@gmail.com>
2025-10-24 15:47:59 +0200

    Added standardized notifications (notify-send)

--- plugins/power_max_current/power_max_current.plugin.zsh
+++ plugins/power_max_current/power_max_current.plugin.zsh
@@ -11,10 +11,13 @@ power_max_current() {
     if grep -Eq "^[#[:space:]]*usb_max_current_enable=" "$cfg"; then
         # Normalize it — uncomment and set to 1
         sudo sed -i 's/^[#[:space:]]*usb_max_current_enable=.*/usb_max_current_enable=1/' "$cfg"
-        echo "Maximum current setting corrected and activated."
+        echo "🎉 Maximum current setting corrected and activated"
+        notify-send --app-name 'AstroArch' --icon="/home/astronaut/.astroarch/assets/icons/novnc-icon.svg" -t 10000 'Maximum Current' "🎉 Maximum current setting corrected and activated"
     else
         # Add it right after [pi5]
         sudo sed -i '/^\[pi5\]/a usb_max_current_enable=1' "$cfg"
         echo "Maximum current activation line added under [pi5]."
+        echo "✅ Maximum current activation line added under [pi5] is done after restart"
+        notify-send --app-name 'AstroArch' --icon="/home/astronaut/.astroarch/assets/icons/novnc-icon.svg" -t 10000 'Maximum Current' "✅ Maximum current activation line added under [pi5] is done after restart"
     fi
 }
