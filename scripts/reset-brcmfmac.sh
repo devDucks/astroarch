500a68e3479f11408497bd92a107b92b69e7f948
sc74 <scarlin74@gmail.com>
2026-03-16 03:07:08 +0100

    Kiosk update

--- scripts/reset-brcmfmac.sh
+++ scripts/reset-brcmfmac.sh
@@ -1,17 +1,31 @@
 #!/bin/zsh
 
-sudo systemctl stop wpa_supplicant
-sudo systemctl stop NetworkManager
-sudo systemctl stop x0vncserver
-sudo killall wpa_supplicant
-sudo rfkill unblock all
-sudo modprobe -r brcmfmac_wcc
-sudo modprobe -r brcmfmac
-sudo modprobe brcmfmac
-sudo systemctl start wpa_supplicant
-sudo systemctl start NetworkManager
-sudo systemctl start x0vncserver
-sudo /home/astronaut/.astroarch/scripts/create_ap.sh
-echo "brcmfmac wifi driver is reset. Your Raspberry must be restarted"
-read -p "Press enter to continue"
+notify-send --app-name 'AstroArch' \
+    --icon="/home/astronaut/.astroarch/assets/icons/novnc-icon.svg" \
+    -t 15000 'AstroArch WIFI' "❌ The BRCMFMAC Wi-Fi driver is out of service. To perform the repair, your RPI must restart"
+
+CONN_NAME="astroarch-hotspot"
+
+# End of services
+systemctl stop NetworkManager wpa_supplicant x0vncserver
+killall -9 wpa_supplicant 2>/dev/null
+
+# Delete ALL hotspot connections
+for uuid in $(nmcli -g UUID,NAME connection show | grep "$CONN_NAME" | cut -d: -f1); do
+    nmcli connection delete uuid "$uuid"
+done
+
+rfkill unblock all
+modprobe -r brcmfmac_wcc 2>/dev/null
+modprobe -r brcmfmac 2>/dev/null
+sleep 2
+modprobe brcmfmac
+sleep 2
+
+# Restarting services
+systemctl start wpa_supplicant
+systemctl start NetworkManager
+systemctl start x0vncserver
+sleep 3
+
 reboot
